4f8919a997be215fa2bc923331e19bfdde485fa6
Stefan Zivanov <zivanovstefan@gmail.com>
2022-02-23 20:12:29 +0100

    Added a new LinkedStack with AddNode, Peek and Print methods.

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LinkedList;
+
+var linkedStack = new LinkedStack<int>();
+linkedStack.AddNode(1);
+linkedStack.AddNode(2);
+linkedStack.AddNode(3);
+linkedStack.AddNode(4);
+linkedStack.AddNode(5);
+
+Console.WriteLine($"Display last added value : {linkedStack.Peek()}");
+linkedStack.Print();
