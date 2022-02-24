09e21c6f04e06c0cf2889b63e62ee91a4d58bb19
Stefan Zivanov <zivanovstefan@gmail.com>
2022-02-24 11:43:58 +0100

    Created new class Node and solved task without LinkedList data structure.

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,11 +1,44 @@
-﻿using LinkedList;
+﻿using System;
+using LinkedList;
 
-var linkedStack = new LinkedStack<int>();
-linkedStack.AddNode(1);
-linkedStack.AddNode(2);
-linkedStack.AddNode(3);
-linkedStack.AddNode(4);
-linkedStack.AddNode(5);
+class Program
+{
+    public Node head;
+    public void AddNode(int new_data)
+    {
+        Node new_node = new Node(new_data);
+        new_node.next = head;
+        head = new_node;
+    }
+    public void Peek()
+    {
+        Node lastnode = head;
+            Console.Write(lastnode.data);
+    }
+    public void Print()
+    {
+        Node tnode = head;
+        while (tnode != null)
+        {
+            Console.Write(tnode.data + " ");
+            tnode = tnode.next;
+        }
+    }
+    public static void Main(String[] args)
+    {
+        Program llist = new Program();
 
-Console.WriteLine($"Display last added value : {linkedStack.Peek()}");
-linkedStack.Print();
+        llist.AddNode(7);
+        llist.AddNode(1);
+        llist.AddNode(3);
+        llist.AddNode(5);
+        llist.AddNode(6);
+        llist.AddNode(1);
+
+        Console.Write("Created Linked list is: ");
+        llist.Print();
+        Console.Write("\n\nLast added value is:");
+        llist.Peek();
+        Console.Write("\n");
+    }
+}
